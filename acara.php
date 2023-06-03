6c383b78a3c027254b0a43dc1a7464828dd5b67b
myportfolioferryawan <ferryawan.a@students.esqbs.ac.id>
2023-06-03 13:39:32 +0700

    update bugs

--- acara.php
+++ acara.php
@@ -71,7 +71,7 @@ $data = mysqli_query($mysqli, "SELECT a.nama, a.tanggal, a.deskripsi, ra.id_acar
         <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
           <!-- Sidebar Toggle (Topbar) -->
           <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-            <i class="fa fa-bars">#</i>
+            <i class="fa fa-bars"></i>
           </button>
           <!-- Topbar Navbar -->
           <ul class="navbar-nav ml-auto">
--- database.php
+++ database.php
@@ -1,23 +1,21 @@
 <?php
-class Database {
-    private $host = "localhost";
-    private $username = "root";
-    private $password = "";
-    private $database = "voluntter_ats";
-    private $conn;
+// class Database {
+//     private $host = "localhost";
+//     private $username = "root";
+//     private $password = "";
+//     private $database = "voluntter_ats";
+//     private $conn;
     
-    public function __construct() {
-        $this->conn = new mysqli($this->host, $this->username, $this->password, $this->database);
-        if($this->conn->connect_error) {
-            die("Connection failed: " . $this->conn->connect_error);
-        }
-    }
-    
-    public function getConnection() {
-        return $this->conn;
-    }
-}
-
+//     public function __construct() {
+//         $this->conn = new mysqli($this->host, $this->username, $this->password, $this->database);
+//         if($this->conn->connect_error) {
+//             die("Connection failed: " . $this->conn->connect_error);
+//         }
+//     }  
+//     public function getConnection() {
+//         return $this->conn;
+//     }
+// }
 // $database = new Database();
 // $conn = $database->getConnection();
 
